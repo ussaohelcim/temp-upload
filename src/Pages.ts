00f2b45074f89f6ffa55d9b1de6371be3d96c670
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-06-04 21:27:26 -0300

    added Pages.ts

---
+++ src/Pages.ts
@@ -0,0 +1,18 @@
+export interface ILinkPage{
+	link:string
+}
+
+export interface IUploadPage{
+	title:string
+	logoImage:string
+	uploadUrl:string
+	fieldName:string
+	emptySpace:string
+	fileAmountNOW:string
+	fileAmountALL:string
+	fileSize:string
+}
+
+export interface IErrorPage{
+	errorMessage:string
+}
