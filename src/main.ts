25c285c77e5a3200e40a7ea7c827ea966a229014
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-06-05 01:01:11 -0300

    changed link url to use req.headers.origin

--- src/main.ts
+++ src/main.ts
@@ -129,7 +129,7 @@ app.post('/upload',async (req,res)=>{
 						console.log(req.file?.filename,"added to db")
 
 						const conf:ILinkPage = {
-							link:`${serverConfig.hostname}:${serverConfig.serverPort}/${secret}/${req.body.password}`
+							link:`${req.headers.origin}/${secret}/${req.body.password}`
 						}
 
 						res.render('linkpage',conf)
